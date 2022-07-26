defa550394508acf28a0b3131043bc16792d42b0
cast10 <castanha.douglas2013@gmail.com>
2022-07-26 17:26:20 -0300

    cadastro de vendedor v1

--- src/Desafio/Vendedor.java
+++ src/Desafio/Vendedor.java
@@ -1,29 +1,67 @@
 package Desafio;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Vendedor {
-    private String nome;
-    private int cpf;
-    private String  email;
 
-    public Vendedor(String nome, int cpf, String email) {
-        this.nome = nome;
+    private String cpf;
+    private String  email;
+    Scanner input = new Scanner(System.in);
+    ArrayList<Vendedor>ListaVendedor = new ArrayList<>();
+    private boolean continuarCadastrandoVendedo =true;
+
+    public Vendedor(String cpf, String email) {
         this.cpf = cpf;
         this.email = email;
+
+
+    }/// adicionar
+
+    public void adiconaVendedor(){
+
+        System.out.println("Qual é seu CPF ?  ");
+        String  cpf = input.next();
+
+        System.out.println("Qual é seu EMAIL ?  ");
+        String email = input.next();
+
+        System.out.println("Foi adicionado um vendedor");
+
+        Vendedor novoVendedor = new Vendedor(cpf, email);
+        ListaVendedor.add(novoVendedor);
+        continuarCadastrandoVendedor();
+
+    }
+    public void continuarCadastrandoVendedor(){
+        Scanner loop = new Scanner(System.in);
+        while (continuarCadastrandoVendedo){
+            System.out.println("Deseja cadastrar mais vendedor ? S / N ");
+            String cadastro = loop.next();
+            if (cadastro.equalsIgnoreCase("S")){
+                adiconaVendedor();
+            }else {
+                continuarCadastrandoVendedo = false;
+            }
+        }
+
     }
 
-    public String getNome() {
-        return nome;
+
+    /// Exibir
+    public void exibirVendedor(){
+
+        for (int i =0; i < ListaVendedor.size(); i++)
+            System.out.println("Número do CPF  "   +  ListaVendedor.get(i).getCpf()+ "Email" + ListaVendedor.get(i).getEmail());
+
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
-    public int getCpf() {
+    public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(int cpf) {
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
@@ -35,3 +73,5 @@ public class Vendedor {
         this.email = email;
     }
 }
+
+
---
+++ src/Desafio/mainteste.java
@@ -0,0 +1,12 @@
+package Desafio;
+
+public class mainteste {
+
+    public static void main(String[] args) {
+
+        Vendedor v1 = new Vendedor("174", "jumento@jumento");
+        v1.adiconaVendedor();
+        v1.exibirVendedor();
+
+    }
+}
