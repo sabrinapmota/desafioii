a483816cd2541e13cca3fff7237720c43f6d8593
cast10 <castanha.douglas2013@gmail.com>
2022-07-26 13:31:39 -0300

    cadastro de vendedor

--- src/Desafio/Vendedor.java
+++ src/Desafio/Vendedor.java
@@ -4,4 +4,34 @@ public class Vendedor {
     private String nome;
     private int cpf;
     private String  email;
+
+    public Vendedor(String nome, int cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
