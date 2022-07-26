8e56c752d3accc620b781ecb65ab522bc115d64b
sabrinaporto <sabrinapmota@yahoo.com.br>
2022-07-26 18:09:28 -0300

    Classe realiza vendas implementada

--- src/Desafio/RealizaVenda.java
+++ src/Desafio/RealizaVenda.java
@@ -4,40 +4,56 @@ package Desafio;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class RealizaVenda implements Venda{
+public class RealizaVenda extends Registro {
 
 
-
+    private String nomeCliente;
     private double valorVenda;
     private String dataVenda;
 
 
-    ArrayList<RealizaVenda> realizaVenda = new ArrayList<>();
-
-    Scanner carrinho = new Scanner(System.in);
-
-    public RealizaVenda(double valorVenda, String dataVenda) {
+    public RealizaVenda(String cpf, String email, String nomeCliente, double valorVenda, String dataVenda) {
+        super(cpf, email);
+        this.nomeCliente = nomeCliente;
         this.valorVenda = valorVenda;
         this.dataVenda = dataVenda;
     }
 
-    public void cadastraVenda(){
-        System.out.println("Informe data da venda: ");
-        System.out.println("Nome do Vendedor: ");
-        System.out.println("Nome do Cliente");
+    ArrayList<RealizaVenda> realizaVenda = new ArrayList<>();
+
+    Scanner input = new Scanner(System.in);
+
+
+    public void cadastraVenda() {
+
+        System.out.println("Informe o nome do cliente: " + nomeCliente + input.nextLine());
+
+        System.out.println("Informe data da venda: " + dataVenda + input.nextLine());
+
+
+        System.out.println("Informe o valor da venda:" + valorVenda + input.nextLine());
 
 
     }
 
-    public void calculaVenda (){
-        System.out.println("O valor da venda foi:Informe o nome do produto: ");
+    boolean executar = true;
+    int opcao = input.nextInt();
 
-        System.out.println("Informe a quantidade: ");
+    {
+        while (executar) {
+            System.out.println("deseja cadastrar essa venda? Digite 1 para sim e 2 para não: ");
+            if (opcao == 1) {
+                System.out.println("Sua venda foi cadastrada com sucesso! ");
 
-        System.out.println("Informe o valor");
+            } else if (opcao == 2) {
+                System.out.println("Venda não cadastrada");
 
+            } else {
+                System.out.println("Digite uma opção válida!");
+            }
+            executar = false;
+        }
 
     }
-
 
 }
