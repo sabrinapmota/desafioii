1982a90760b8156d8cc664b1a44877f875b29807
cast10 <castanha.douglas2013@gmail.com>
2022-07-27 12:43:27 -0300

    realizado Implementação de listas de Vendedor e CPFs

--- src/Desafio/Vendedor.java
+++ src/Desafio/Vendedor.java
@@ -1,38 +1,60 @@
 package Desafio;
+import com.sun.management.UnixOperatingSystemMXBean;
+
+import java.sql.ClientInfoStatus;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Vendedor {
-
+    private String nomeVendedor;
     private String cpf;
     private String  email;
     Scanner input = new Scanner(System.in);
     ArrayList<Vendedor>ListaVendedor = new ArrayList<>();
     private boolean continuarCadastrandoVendedo =true;
 
-    public Vendedor(String cpf, String email) {
+    public Vendedor(String cpf, String email, String nomeVendedor) {
         this.cpf = cpf;
         this.email = email;
+        this.nomeVendedor = nomeVendedor;
 
 
     }/// adicionar
 
     public void adiconaVendedor(){
+        System.out.println( "Digite nome do vendedor(a)");
+        nomeVendedor = input.nextLine();
 
-        System.out.println("Qual é seu CPF ?  ");
-        String  cpf = input.next();
+        System.out.println("Digite o CPF do vendedor (a) ?  ");
+        cpf = input.nextLine();
+        for (int i = 0; i < ListaVendedor.size(); i++) {
+            if (ListaVendedor.contains(cpf)){
+                System.out.println("Esse CPF já existe na base dados, por favor informar novamente ");
+                cpf =input.nextLine();
+            }
+        }
 
-        System.out.println("Qual é seu EMAIL ?  ");
-        String email = input.next();
+        System.out.println("Digite o email do vendedor (a) ? ");
+        email = input.next();
+        for (int i = 0; i < ListaVendedor.size() ; i++) {
+            if(ListaVendedor.contains(email)){
+                System.out.println("Esse email já existe na base dados, por favor informar novamente ");
+                 email = input.next();
+            }
+        }
 
-        System.out.println("Foi adicionado um vendedor");
-
-        Vendedor novoVendedor = new Vendedor(cpf, email);
-        ListaVendedor.add(novoVendedor);
-        continuarCadastrandoVendedor();
+        if (email.contains("@") && email.contains(".com") || email.contains(".br")){
+            Vendedor vendedor1= new Vendedor(cpf, email,nomeVendedor);
+            ListaVendedor.add(vendedor1);
+        }else {
+            System.out.println("Email está incorreto Favor verificar e informar novamente  ");
+            System.out.println("");
+            System.out.println("Cadastro não realizado");
+        }
 
     }
+
     public void continuarCadastrandoVendedor(){
         Scanner loop = new Scanner(System.in);
         while (continuarCadastrandoVendedo){
@@ -44,18 +66,14 @@ public class Vendedor {
                 continuarCadastrandoVendedo = false;
             }
         }
-
     }
-
-
     /// Exibir
     public void exibirVendedor(){
-
+        System.out.println("LIsta de vendedores cadastrados ");
         for (int i =0; i < ListaVendedor.size(); i++)
-            System.out.println("Número do CPF  "   +  ListaVendedor.get(i).getCpf()+ "Email" + ListaVendedor.get(i).getEmail());
+            System.out.println("Nome "+ ListaVendedor.get(i).nomeVendedor+"Número do CPF  "   +  ListaVendedor.get(i).getCpf()+ "Email" + ListaVendedor.get(i).getEmail());
 
     }
-
 
     public String getCpf() {
         return cpf;
@@ -71,5 +89,13 @@ public class Vendedor {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getNomeVendedor() {
+        return nomeVendedor;
+    }
+
+    public void setNomeVendedor(String nomeVendedor) {
+        this.nomeVendedor = nomeVendedor;
     }
 }
